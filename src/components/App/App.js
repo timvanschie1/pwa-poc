4532fe92b5b852bf7147dce3767536af65f31aec
timvanschie1 <timvanschie1@gmail.com>
2019-11-19 22:51:52 +0100

    Made sure the app still works when browser doesn't support the cache api Also show which api's are supported in the current browser

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,13 @@ import './App.scss';
 function App() {
     const MAX_COMIC_NR = 2228;
 
+    const [cacheIsSupported, setCacheIsSupported] = useState('caches' in self);
+    const [swIsSupported, setSwIfSupported] = useState('serviceWorker' in navigator);
+
     const [comicId, setComicId] = useState('1');
     const [comic, setComic] = useState({});
-
     const [nrOfCachedComics, setNrOfCachedComics] = useState(null);
     const [cachedComics, setCachedComics] = useState([]);
-
     const [lastFetchSucceeded, setLastFetchSucceeded] = useState(true);
 
     useEffect(() => {
@@ -22,6 +23,7 @@ function App() {
 
     function fetchComic() {
         const showLoaderAfterShortTime = window.setTimeout(() => setNrOfCachedComics(null), 100);
+
         fetch('https://xkcd.now.sh/?comic=' + comicId, {mode: 'cors'})
             .then(response => {
                 if (!response.ok) {
@@ -44,6 +46,10 @@ function App() {
     }
 
     function updateCacheInfo() {
+        if (!cacheIsSupported) {
+            return;
+        }
+
         window.caches.open('pwa-poc-xkcd').then(cache => {
             cache.keys()
                 .then(keys => {
@@ -58,8 +64,6 @@ function App() {
                             setNrOfCachedComics(responses.length);
                         }
                     );
-
-                    setNrOfCachedComics(cachedComicsRequests.length);
                 });
         })
     }
@@ -85,7 +89,6 @@ function App() {
     function cacheBunchOfComics() {
         setNrOfCachedComics(null);
         for (let i = 1; i <= 100; i++) {
-            console.log('cache', i);
             fetch('https://xkcd.now.sh/?comic=' + i, {mode: 'cors'})
                 .then(response => {
                     if (!response.ok) {
@@ -111,70 +114,97 @@ function App() {
         e.preventDefault();
     }
 
+    const renderCacheInfo = (
+        <div className="cache">
+            {nrOfCachedComics === null
+                ? (
+                    <h2 className='loading'>
+                        <span className="loading__dot loading__dot--1">.</span>
+                        <span className="loading__dot loading__dot--2">.</span>
+                        <span className="loading__dot loading__dot--3">.</span>
+                    </h2>
+                ) : <h2>{nrOfCachedComics}</h2>
+            }
+            <p className="cache__info">In cache</p>
+            <button onClick={emptyCache}>Empty</button>
+            <button onClick={cacheBunchOfComics}>Cache 100</button>
+        </div>
+    );
+
+    const renderForm = (
+        <form className="search" onSubmit={handleSubmit}>
+            <label className="search__label" htmlFor="search">#</label>
+            <input id="search"
+                   className="search__field"
+                   type="number"
+                   value={comicId}
+                   min="0"
+                   max={MAX_COMIC_NR}
+                   autoFocus
+                   pattern="\d*"
+                   onChange={e => setComicId(e.target.value)}/>
+            <button disabled className="search__button">
+                <img src="./assets/search.svg" alt="Zoeken"/>
+            </button>
+        </form>
+    );
+
+    const renderComic = (
+        <div className="comic">
+            <h1 className="comic__title">{comic.title}</h1>
+            <img className="comic__image" src={comic.img} alt=""/>
+            <p className="comic__description">
+                {comic.alt}
+                <span className="comic__year">{comic.year}</span>
+            </p>
+        </div>
+    );
+
+    const renderOfflineFallback = (
+        <div className="offline">
+            <h1 className="offline__title">Je bent offline</h1>
+            {cacheIsSupported
+                ? (
+                    <React.Fragment>
+                        <p>Deze eerder opgeslagen comics kun je wel bekijken:</p>
+                        <ul className="offline__cached">
+                            {cachedComics.map(cachedComic =>
+                                <li>
+                                    <a href="#" onClick={e => {
+                                        e.preventDefault();
+                                        setComicId(cachedComic.num);
+                                    }}>
+                                        <img src={cachedComic.img} alt=""/>
+                                        <span className="offline__cached-nr">{cachedComic.num}</span>
+                                    </a>
+                                </li>
+                            )}
+                        </ul>
+                    </React.Fragment>
+                ) : ''
+            }
+        </div>
+    );
+
+    const renderApiSupport = (
+        <div className="api-support">
+            <h2>This browser supports</h2>
+            <div>Service Worker: <span>{swIsSupported ? 'YES' : 'NO'}</span></div>
+            <div>Cache API: <span>{cacheIsSupported ? 'YES' : 'NO'}</span></div>
+        </div>
+    );
+
     return (
         <div className="App">
-            <div className="cache">
-                {nrOfCachedComics === null
-                    ? (
-                        <h2 className='loading'>
-                            <span className="loading__dot loading__dot--1">.</span>
-                            <span className="loading__dot loading__dot--2">.</span>
-                            <span className="loading__dot loading__dot--3">.</span>
-                        </h2>
-                    ) : <h2>{nrOfCachedComics}</h2>
-                }
-                <p className="cache__info">In cache</p>
-                <button onClick={emptyCache}>Empty</button>
-                <button onClick={cacheBunchOfComics}>Cache 100</button>
-            </div>
-
-            <form className="search" onSubmit={handleSubmit}>
-                <label className="search__label" htmlFor="search">#</label>
-                <input id="search"
-                       className="search__field"
-                       type="number"
-                       value={comicId}
-                       min="0"
-                       max={MAX_COMIC_NR}
-                       autoFocus
-                       pattern="\d*"
-                       onChange={e => setComicId(e.target.value)}/>
-                <button disabled className="search__button">
-                    <img src="./assets/search.svg" alt="Zoeken"/>
-                </button>
-            </form>
+            {cacheIsSupported ? renderCacheInfo : ''}
+            {renderForm}
             {comic
                 ? lastFetchSucceeded
-                    ? (
-                        <div className="comic">
-                            <h1 className="comic__title">{comic.title}</h1>
-                            <img className="comic__image" src={comic.img} alt=""/>
-                            <p className="comic__description">
-                                {comic.alt}
-                                <span className="comic__year">{comic.year}</span>
-                            </p>
-                        </div>
-                    ) : (
-                        <div className="offline">
-                            <h1 className="offline__title">Je bent offline</h1>
-                            <p>Deze eerder opgeslagen comics kun je wel bekijken:</p>
-                            <ul className="offline__cached">
-                                {cachedComics.map(cachedComic =>
-                                    <li>
-                                        <a href="#" onClick={e => {
-                                            e.preventDefault();
-                                            setComicId(cachedComic.num);
-                                        }}>
-                                            <img src={cachedComic.img} alt=""/>
-                                            <span className="offline__cached-nr">{cachedComic.num}</span>
-                                        </a>
-                                    </li>
-                                )}
-                            </ul>
-                        </div>
-                    )
+                    ? renderComic
+                    : renderOfflineFallback
                 : ''
             }
+            {renderApiSupport}
         </div>
     );
 }
